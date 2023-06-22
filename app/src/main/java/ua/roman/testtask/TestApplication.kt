5d08643a08eb0dc8bc4532ebe9f629b067d91ca8
RogdanWork <rogdan.work@gmail.com>
2023-06-22 11:44:43 +0300

    Application initialization

---
+++ app/src/main/java/ua/roman/testtask/TestApplication.kt
@@ -0,0 +1,10 @@
+package ua.roman.testtask
+
+import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+class TestApplication : Application()
